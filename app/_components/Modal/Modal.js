69729a5f4822336b2997e6a4abe29e7bf9c6810d
susu725 <zxcv1010017@gmail.com>
2023-12-14 03:57:42 +0800

    feat: modal RWD

--- app/_components/Modal/Modal.js
+++ app/_components/Modal/Modal.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { faDownload, faShare, faCircleInfo, faXmark } from '@fortawesome/free-solid-svg-icons'
 import styles from './Modal.module.scss'
 
@@ -20,7 +21,6 @@ export default function Modal({ handleClose, photo }) {
                         <p>Share</p>
                         <FontAwesomeIcon icon={faShare} className={styles.icon} />
                     </button>
-
                     <button>
                         <p>Info</p>
                         <FontAwesomeIcon icon={faCircleInfo} className={styles.icon} />
@@ -31,7 +31,22 @@ export default function Modal({ handleClose, photo }) {
                 </button>
             </div>
             <img src={url} alt='#' className={styles.img} />
-            <p className={styles.title}>{title}</p>
+            <div className={styles.imgInfo}>
+                <p className={styles.title}>
+                    <span>Title</span>
+                    {title}
+                </p>
+                <div className={styles.userAction}>
+                    <button>
+                        <p>Like</p>
+                        <FontAwesomeIcon icon={far.faHeart} className={styles.icon} />
+                    </button>
+                    <button>
+                        <p>Save</p>
+                        <FontAwesomeIcon icon={far.faBookmark} className={styles.icon} />
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
