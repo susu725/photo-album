4c5d1d0bdc93f4c68b7b642e03632ad5327c25c0
susu725 <zxcv1010017@gmail.com>
2023-12-13 20:36:48 +0800

    feat: navbar layout

--- app/_components/Navbar/Navbar.js
+++ app/_components/Navbar/Navbar.js
@@ -1,9 +1,24 @@
-'use client';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faPlus, faBell } from '@fortawesome/free-solid-svg-icons'
 import styles from './Navbar.module.scss'
 
 export default function Navbar() {
     return (
-        <p>Navbar</p>
+        <div className={styles.navbar}>
+            <div>
+                <button className={styles.search}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+                </button>
+            </div>
+            <div className={styles.navR}>
+                <button className={styles.plus}>
+                    <p>Submit a photo</p>
+                    <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+                </button>
+                <button className={styles.notify}>
+                    <FontAwesomeIcon icon={faBell} className={styles.icon} />
+                </button>
+            </div>
+        </div>
     )
 }
